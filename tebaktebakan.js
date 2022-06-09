943b074106cdc5a011df91b82c4dbc145e971aff
kari-basi <87919684+kari-basi@users.noreply.github.com>
2022-06-09 19:47:11 +0800

    Update tebaktebakan.js

--- tebaktebakan.js
+++ tebaktebakan.js
@@ -5,17 +5,17 @@ module.exports = {
     alias: ["tebakan"],
     desc: "Entertaiment Tebak Tebakan",
     type: "entertainment",
-    start: async(killua, m) => {
+    start: async(GojoMdNx, m) => {
         if (tebaktebakan.hasOwnProperty(m.sender.split('@')[0])) return m.reply("Masih Ada Sesi Yang Belum Diselesaikan!")
         let fetch = await fetchUrl(global.api("zenz", "/api/tebaktebakan", {}, "apikey"))
         let result = await fetch.result
-        killua.sendText(m.from, `Silahkan Jawab Pertanyaan Berikut\n\n${result.soal}\n\nWaktu : 30s`, m).then(() => {
+        GojoMdNx.sendText(m.from, `Silahkan Jawab Pertanyaan Berikut\n\n${result.soal}\n\nWaktu : 30s`, m).then(() => {
             tebaktebakan[m.sender.split('@')[0]] = result.jawaban.toLowerCase()
             console.log("Jawaban: " + result.jawaban)
         })
         await sleep(30000)
         if (tebaktebakan.hasOwnProperty(m.sender.split('@')[0])) {
-            killua.sendText(m.from, `Waktu Habis\n\nJawaban:  ${tebaktebakan[m.sender.split('@')[0]]}`, m)
+            GojoMdNx.sendText(m.from, `Waktu Habis\n\nJawaban:  ${tebaktebakan[m.sender.split('@')[0]]}`, m)
             delete tebaktebakan[m.sender.split('@')[0]]
         }
     }
