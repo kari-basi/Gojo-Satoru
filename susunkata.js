6d09cc32ba306b9155bbb1044ee85837820210bc
kari-basi <87919684+kari-basi@users.noreply.github.com>
2022-06-09 19:46:02 +0800

    Update susunkata.js

--- susunkata.js
+++ susunkata.js
@@ -5,17 +5,17 @@ module.exports = {
     alias: ["skata"],
     desc: "Entertaiment Susun Kata",
     type: "entertainment",
-    start: async(killua, m) => {
+    start: async(GojoMdNx, m) => {
         if (susunkata.hasOwnProperty(m.sender.split('@')[0])) return m.reply("Masih Ada Sesi Yang Belum Diselesaikan!")
         let fetch = await fetchUrl(global.api("zenz", "/api/susunkata", {}, "apikey"))
         let result = await fetch.result
-        killua.sendText(m.from, `Silahkan Jawab Pertanyaan Berikut\n\n${result.soal}\nTipe: ${result.tipe}\n\nWaktu : 30s`, m).then(() => {
+        GojoMdNx.sendText(m.from, `Silahkan Jawab Pertanyaan Berikut\n\n${result.soal}\nTipe: ${result.tipe}\n\nWaktu : 30s`, m).then(() => {
             susunkata[m.sender.split('@')[0]] = result.jawaban.toLowerCase()
             console.log("Jawaban: " + result.jawaban)
         })
         await sleep(30000)
         if (susunkata.hasOwnProperty(m.sender.split('@')[0])) {
-            killua.sendText(m.from, `Waktu Habis\n\nJawaban:  ${susunkata[m.sender.split('@')[0]]}`, m)
+            GojoMdNx.sendText(m.from, `Waktu Habis\n\nJawaban:  ${susunkata[m.sender.split('@')[0]]}`, m)
             delete susunkata[m.sender.split('@')[0]]
         }
     }
