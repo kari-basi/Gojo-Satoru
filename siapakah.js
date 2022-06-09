c0ca563c4ce1cc1173d7eca6cae35ca36e869af0
kari-basi <87919684+kari-basi@users.noreply.github.com>
2022-06-09 19:46:30 +0800

    Update siapakah.js

--- siapakah.js
+++ siapakah.js
@@ -9,7 +9,7 @@ module.exports = {
         if (siapakah.hasOwnProperty(m.sender.split('@')[0])) return m.reply("Masih Ada Sesi Yang Belum Diselesaikan!")
         let fetch = await fetchUrl(global.api("zenz", "/api/siapakah", {}, "apikey"))
         let result = await fetch.result
-        killua.sendText(m.from, `Silahkan Jawab Pertanyaan Berikut\n\n${result.soal}\n\nWaktu : 30s`, m).then(() => {
+        GojoMdNx.sendText(m.from, `Silahkan Jawab Pertanyaan Berikut\n\n${result.soal}\n\nWaktu : 30s`, m).then(() => {
             siapakah[m.sender.split('@')[0]] = result.jawaban.toLowerCase()
             console.log("Jawaban: " + result.jawaban)
         })
