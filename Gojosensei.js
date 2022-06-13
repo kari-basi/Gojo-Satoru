e851d66e0ebf4a586b2664dd0924b61cde5b37c8
kari-basi <87919684+kari-basi@users.noreply.github.com>
2022-06-13 15:16:12 +0800

    Update Gojosensei.js

--- Gojosensei.js
+++ Gojosensei.js
@@ -3087,6 +3087,38 @@ ${id}`)
    		 },
    		 isQuery: true
 	    }
+	break
+	case 'nhpdf' :
+	try {
+	let count = 0
+        let pages = []
+        const get_result = await fetchUrl(global.api("zenz", "/animeweb/nhentai", { query: text }, "apikey"))
+        const doujin = await get_result.result
+        const array_page = doujin.image
+        const titles = doujin.title.pretty
+        const title = titles.replace(/\s+/g, '')
+        m.reply('Downloading...')
+        for (let index = 0; index < array_page.length; index++) {
+            const image_name = "temp/" + title + index + ".png"
+            await new Promise((resolve) => request(array_page[index]).pipe(fs.createWriteStream(image_name)).on('finish', resolve))
+            count++
+            pages.push(image_name);
+        }
+        await new Promise((resolve) =>
+            topdf(pages, 'A4')
+            .pipe(fs.createWriteStream('temp/' + title + '.pdf'))
+            .on('finish', resolve)
+        )
+        for (let index = 0; index < array_page.length; index++) {
+            fs.unlink("temp/" + title + index + ".png", (err) => {
+                if (err) throw err
+            })
+        }
+        GojoMdNx.sendFile(m.from, fs.readFileSync('temp/' + title + '.pdf'), `${title}.pdf`, m, { asDocument: true, thumbnail: doujin.image[0] })
+        fs.unlinkSync('temp/' + title + '.pdf');
+    },
+    isQuery: true
+}
 		break
 		   case 'bass': case 'blown': case 'deep': case 'earrape': case 'fast': case 'fat': case 'nightcore': case 'reverse': case 'robot': case 'slow': case 'smooth': case 'squirrel':
                 try {
