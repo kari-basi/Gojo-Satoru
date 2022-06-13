d8fdff515ad718557e91176e3e991bedf1953c35
kari-basi <87919684+kari-basi@users.noreply.github.com>
2022-06-13 14:48:35 +0800

    Update Gojosensei.js

--- Gojosensei.js
+++ Gojosensei.js
@@ -3060,6 +3060,34 @@ ${id}`)
 		reply(txt)
 		}
 		break
+	case 'nhentai' : case 'nh' :
+ 		try {
+           		 let fetch = await fetchUrl(global.api("zenz", "/animeweb/nhentai", { query: text }, "apikey"))
+           		 let caption = `Nhentai Search :\n\n`
+            		 let i = fetch.result
+            		 caption += `⭔ ID : ${i.id}\n`
+           		 caption += `⭔ English Title : ${i.title.english ?? ""}\n`
+           		 caption += `⭔ Japanese Title : ${i.title.japanese ?? ""}\n`
+           		 caption += `⭔ Pretty Title : ${i.title.pretty ?? ""}\n`
+            		 caption += `⭔ Image Length : ${i.image.length}\n`
+          		 let buttons = [
+                	     {buttonId: `nhpdf ${text}`, buttonText: { displayText: 'Download PDF'}, type: 1 },
+            		 ]
+            		 let buttonMessage = {
+                	 image: { url: i.image[0] },
+                	 caption: caption,
+                	 footer: config.footer,
+                	 buttons: buttons,
+                	 headerType: 4
+           		 }
+            		 killua.sendMessage(m.from, buttonMessage, { quoted: m })
+        	     } catch {
+            	         global.mess("error", m)
+     		     }
+   		 },
+   		 isQuery: true
+	    }
+		break
 		   case 'bass': case 'blown': case 'deep': case 'earrape': case 'fast': case 'fat': case 'nightcore': case 'reverse': case 'robot': case 'slow': case 'smooth': case 'squirrel':
                 try {
                 let set
